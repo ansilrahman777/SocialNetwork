6d5a33c3de87af243ba90533b711bf9c01f33875
kalai <kalayarasan.titus@gmail.com>
2024-10-31 18:35:03 +0530

    sqllite3 updated in gobika

--- crea/settings.py
+++ crea/settings.py
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'crea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crea',  
-        'USER': 'crea_user',
-        'PASSWORD': 'Gobu@0306',
-        'HOST': 'localhost',
-        'PORT': '3306',            
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'crea',  
+#         'USER': 'crea_user',
+#         'PASSWORD': 'Gobu@0306',
+#         'HOST': 'localhost',
+#         'PORT': '3306',            
+#     }
+# }
 
 
 # Password validation
