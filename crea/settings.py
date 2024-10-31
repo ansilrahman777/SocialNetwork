b523d8ceb813d87df5a342c1b740b2e87f5c82d2
kalai <kalayarasan.titus@gmail.com>
2024-10-31 18:35:42 +0530

    sqllite3 updated in ansil

--- crea/settings.py
+++ crea/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'crea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'creanet_database',  
-        'USER': 'root',
-        'PASSWORD': 'ansil',
-        'HOST': 'localhost',
-        'PORT': '3306',            
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'creanet_database',  
+#         'USER': 'root',
+#         'PASSWORD': 'ansil',
+#         'HOST': 'localhost',
+#         'PORT': '3306',            
+#     }
+# }
 
 
 # Password validation
